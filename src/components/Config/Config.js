7fed03482aebe4b59be977119c61f0931dd17509
xsami <samil.vargas@hotmail.com>
2020-01-14 23:46:41 -0400

    code refactoring and documentation

--- src/components/Config/Config.js
+++ src/components/Config/Config.js
@@ -1,50 +1,30 @@
 import React, { useContext } from 'react';
 import './Config.css';
 import { GameContext } from '../../context';
+import { getNewBoard } from '../../utilities/helpers';
 
-function Position(x=0, y=0) {
-    this.x = x;
-    this.y = y;
-}
-
-function Piece(color='', state=false, moves=[], jumps=[], position={}) {
-    this.color = color;
-    this.state = state;
-    this.moves = moves;
-    this.jumps = jumps;
-    this.position = position;
-}
-
-function getNewBoard(initialBoard){
-
-    const newBoard = [];
-
-    for (let i = 0; i < initialBoard.length; i++) {
-        const newRow = [...initialBoard[i]];
-        
-        for (let j = 0; j < newRow.length; j++) {
-            const state = !(j % 2 ^ (i+1) % 2); // state 
-            const color = (i < 3) ? 'white' : ((i > 4) ? 'red' : ''); // color
-            newRow[j] = new Piece(color, state, [], [], new Position(i, j));
-        }
-        newBoard.push(newRow);
-    }
-
-    return newBoard;
-}
 
 function Config() {
-    const gameContext = useContext(GameContext);
 
-    const {board, setBoard } = gameContext;
-
+    const {board, setBoard, selectedPiece} = useContext(GameContext);
+    
     return (
       <div className="config inline">
-          <h1>Checkers</h1>
-          <code>Made by: xsami</code>
-          <button className="play" onClick={() => setBoard(getNewBoard(board))}>
-              Start
-          </button>
+          
+          <div className="header">
+            <h1>Checkers</h1>
+            <code>Made by: xsami</code>
+            <button className="play" onClick={() => setBoard(getNewBoard(board))}>
+                Start
+            </button>
+          </div>
+          
+          <div className="details">
+              <h2>Details</h2>
+              {
+                selectedPiece.state ? <code>Possible moves</code> : <div>something</div>
+              }
+          </div>
       </div>
     );
   }
---
+++ src/utilities/helpers.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * @param {integer} x 
+ * @param {integer} y
+ * 
+ * Position data structure 
+ */
+export const Position = function(x=0, y=0) {
+    this.x = x;
+    this.y = y;
+};
+
+/**
+ * 
+ * @param {string} color 
+ * @param {boolean} state 
+ * @param {array} moves 
+ * @param {array} jumps 
+ * @param {Position object} position 
+ * 
+ * Piece data structure
+ */
+export const Piece = function(color='', state=false, moves=[], jumps=[], position={}) {
+    this.color = color;
+    this.state = state;
+    this.moves = moves;
+    this.jumps = jumps;
+    this.position = position;
+};
+
+/**
+ * 
+ * @param {matriz} initialBoard 
+ * Return a new board
+ */
+export const getNewBoard = function(initialBoard){
+
+    const newBoard = [];
+
+    for (let i = 0; i < initialBoard.length; i++) {
+        const newRow = [...initialBoard[i]];
+        
+        for (let j = 0; j < newRow.length; j++) {
+            const state = !(j % 2 ^ (i+1) % 2); // state 
+            const color = (i < 3) ? 'white' : ((i > 4) ? 'red' : ''); // color
+            newRow[j] = new Piece(color, state, [], [], new Position(i, j));
+        }
+        newBoard.push(newRow);
+    }
+
+    return newBoard;
+};
